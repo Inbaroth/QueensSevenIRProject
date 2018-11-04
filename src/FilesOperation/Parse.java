42ef603ddd04a610aa3a73f916b5bead661535cd
moravitan <moravit@post.bgo.ac.il>
2018-11-05 01:29:18 +0200

    3

---
+++ src/FilesOperation/Parse.java
@@ -0,0 +1,14 @@
+package FilesOperation;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Parse {
+
+    // String - doc Id
+    Map <String, DocumentsHashMap> termsMap;
+
+    public Parse() {
+        this.termsMap = new TreeMap<>();
+    }
+}
---
+++ src/FilesOperation/ReadFile.java
@@ -0,0 +1,48 @@
+package FilesOperation;
+
+import java.io.*;
+
+public class ReadFile {
+
+    File folder;
+    Parse parser;
+
+
+    public ReadFile(String path) {
+        folder = new File(path);
+        parser = new Parse();
+        listFilesForFolder(folder);
+    }
+
+    private void listFilesForFolder(File folder) {
+        for (File fileEntry : folder.listFiles()) {
+            if (fileEntry.isDirectory()) {
+                listFilesForFolder(fileEntry);
+            } else {
+                try {
+                    BufferedReader br = new BufferedReader(new FileReader(fileEntry));
+                    String line;
+                    String file = "";
+                    line = br.readLine();
+                    while (line != null){
+                        if (!line.equals("</Docs>")){
+                            file += line;
+                            line = br.readLine();
+                        }
+                        else{
+                            //parser.parse(file)
+                            file = "";
+                        }
+
+
+
+                    }
+
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
